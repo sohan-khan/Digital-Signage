d9ef496feb8bd467c6fabb8bcb46d048d51c54d8
sohan-khan <102828636+sohan-khan@users.noreply.github.com>
2024-05-22 12:43:32 +0600

    first commit

---
+++ Al_Rukn_Sign/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Al_Rukn_Sign.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
